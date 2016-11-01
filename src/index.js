4076530628d3030af0cd4c3e94bb7ffbcc9afaba
olih <olih@users.noreply.github.com>
2016-11-01 21:22:41 +0000

    pick the shortest and longest size

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-/**
- * @param {Type}
- * @return {Type}
- */
-export default function () {
-  return true
+import _ from "lodash"
+
+
+const pickLongestSize = (list) => {
+  if (_.isEmpty(list)) return null;
+  const sorted = _.sortBy(list, s => _.size(s))
+  const maxSize = _.size(_.last(sorted))
+  const filtered = _.filter(list, s => _.size(s) ===  maxSize)
+  return _.head(filtered)
 }
+
+const pickShortestSize = (list) => {
+  if (_.isEmpty(list)) return null;
+  const sorted = _.sortBy(list, s => _.size(s))
+  const minSize = _.size(_.head(sorted))
+  const filtered = _.filter(list, s => _.size(s) ===  minSize)
+  return _.head(filtered)
+}
+
+const sumSize = () => {
+
+}
+
+const minLengthOfStringTemplate = () => {
+
+}
+
+const calculateAltSizeTable = () => {
+
+}
+
+const pickAlternateValue = {
+  pickLongestSize,
+  pickShortestSize,
+  sumSize,
+  minLengthOfStringTemplate,
+  calculateAltSizeTable
+}
+
+module.exports=pickAlternateValue
--- test/index.js
+++ test/index.js
@@ -1,7 +1,31 @@
-import test from "tape"
-import pickAlternateValue from "../src"
+import test from 'tape'
+import pav from '../src'
 
-test("pickAlternateValue", (t) => {
-  t.plan(1)
-  t.equal(true, pickAlternateValue(), "return true")
+
+test('pick the longest size', (t) => {
+  const a = 'A'
+  const b = 'B'
+  const c = 'C'
+  t.plan(7)
+  t.equal(pav.pickLongestSize(null), null, 'null')
+  t.equal(pav.pickLongestSize([]), null, 'empty')
+  t.equal(pav.pickLongestSize(['ab','a','abcd']),'abcd', 'A')
+  t.equal(pav.pickLongestSize(['abcde','a','ab']),'abcde', 'B')
+  t.equal(pav.pickLongestSize(['ab','ab1','ab2','a']),'ab1', 'take 1st of 2 eq')
+  t.deepEqual(pav.pickLongestSize([[1,2],[1,2,3],[1]]),[1,2,3], 'arr of array')
+  t.deepEqual(pav.pickLongestSize([{a,b},{a,b,c},{a}]),{a,b,c}, 'array of obj')
+})
+
+test('pick the shortest size', (t) => {
+  const a = 'A'
+  const b = 'B'
+  const c = 'C'
+  t.plan(7)
+  t.equal(pav.pickShortestSize(null), null, 'null')
+  t.equal(pav.pickShortestSize([]), null, 'empty')
+  t.equal(pav.pickShortestSize(['ab','a','abcd']),'a', 'A')
+  t.equal(pav.pickShortestSize(['abcde','a','ab']),'a', 'B')
+  t.equal(pav.pickShortestSize(['abd','a1','a2','abc']),'a1', 'take 1st of 2 eq')
+  t.deepEqual(pav.pickShortestSize([[1,2],[1,2,3],[1]]),[1], 'arr of array')
+  t.deepEqual(pav.pickShortestSize([{a,b},{a,b,c},{a}]),{a}, 'array of obj')
 })
