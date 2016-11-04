63a590d7b320aceff635513122577b4f962f61ed
olih <olih@users.noreply.github.com>
2016-11-04 23:19:55 +0000

    After eslint cleaning

--- src/index.js
+++ src/index.js
@@ -1,62 +1,62 @@
-import _ from "lodash"
+import _ from 'lodash';
 
 
 const pickLongestSize = (list,
    defaultValue = null,
    max = Number.MAX_SAFE_INTEGER) => {
-  const noNullList = _.filter(list , s => !_.isNil(s) && _.size(s) <= max )
+  const noNullList = _.filter(list, s => !_.isNil(s) && _.size(s) <= max);
   if (_.isEmpty(noNullList)) return defaultValue;
-  const sorted = _.sortBy(noNullList, s => _.size(s))
-  const maxSize = _.size(_.last(sorted))
-  const filtered = _.filter(noNullList, s => _.size(s) ===  maxSize)
-  return _.head(filtered)
-}
+  const sorted = _.sortBy(noNullList, s => _.size(s));
+  const maxSize = _.size(_.last(sorted));
+  const filtered = _.filter(noNullList, s => _.size(s) === maxSize);
+  return _.head(filtered);
+};
 
 const pickShortestSize = (list,
   defaultValue = null,
   min = -1
   ) => {
-  const noNullList = _.filter(list , s => !_.isNil(s) && _.size(s) >= min)
+  const noNullList = _.filter(list, s => !_.isNil(s) && _.size(s) >= min);
   if (_.isEmpty(noNullList)) return defaultValue;
-  const sorted = _.sortBy(noNullList, s => _.size(s))
-  const minSize = _.size(_.head(sorted))
-  const filtered = _.filter(noNullList, s => _.size(s) ===  minSize)
-  return _.head(filtered)
-}
+  const sorted = _.sortBy(noNullList, s => _.size(s));
+  const minSize = _.size(_.head(sorted));
+  const filtered = _.filter(noNullList, s => _.size(s) === minSize);
+  return _.head(filtered);
+};
 
-const sumSize = (list) => {
-  return _.sumBy(list, s => _.size(s))
-}
+const sumSize = list =>
+   _.sumBy(list, s => _.size(s))
+;
 
 const minSize = (list) => {
-  const noNullList = _.filter(list , s => !_.isNil(s))
+  const noNullList = _.filter(list, s => !_.isNil(s));
   if (_.isEmpty(noNullList)) return null;
-  const sorted = _.sortBy(noNullList, s => _.size(s))
-  return _.size(_.head(sorted))
-}
+  const sorted = _.sortBy(noNullList, s => _.size(s));
+  return _.size(_.head(sorted));
+};
 
 const maxSize = (list) => {
-  const noNullList = _.filter(list , s => !_.isNil(s))
+  const noNullList = _.filter(list, s => !_.isNil(s));
   if (_.isEmpty(noNullList)) return null;
-  const sorted = _.sortBy(noNullList, s => _.size(s))
-  return _.size(_.last(sorted))
-}
+  const sorted = _.sortBy(noNullList, s => _.size(s));
+  return _.size(_.last(sorted));
+};
 
 const minLengthOfTemplate = (template) => {
   if (_.isNil(template)) return null;
-  const withoutVars = template.replace(/\$\{\s*[A-Za-z0-9.]+\s*\}/g,'')
+  const withoutVars = template.replace(/\$\{\s*[A-Za-z0-9.]+\s*\}/g, '');
   return _.size(withoutVars);
-}
+};
 
 const getArrInArr = (arrIdx, listOfList) => {
-  if (_.isNil(arrIdx) || _.isNil(listOfList) ) return null;
+  if (_.isNil(arrIdx) || _.isNil(listOfList)) return null;
   const sizeIdx = _.size(arrIdx);
-  const sizeList = _.size(listOfList)
+  const sizeList = _.size(listOfList);
   if (sizeIdx !== sizeList) {
-    throw new Error(`size idx ${sizeIdx} different of ${sizeList}`)
+    throw new Error(`size idx ${sizeIdx} different of ${sizeList}`);
   }
-  var result = [];
-  for (var i=0; i< sizeIdx; i++) {
+  const result = [];
+  for (let i = 0; i < sizeIdx; i += 1) {
     const value = listOfList[i][arrIdx[i]];
     if (_.isUndefined(value)) {
       return null;
@@ -64,33 +64,35 @@ const getArrInArr = (arrIdx, listOfList) => {
     result.push(value);
   }
   return result;
-}
+};
 
 const decArrayIndex = (arrIdx, maxIdx) => {
-  if (sumSize(arrIdx) === 0) return null;
-  const size = _.size(arrIdx);
-  result = [];
-  const headDec = arrIdx[0]--;
-  if (headDec === -1) {
-    const tailIdx = _.tail(arrIdx);
-    const tailMaxIdx = _.tail(maxIdx);
-    const tailRes = decArrayIndex(tailIdx, tailMaxIdx);
-    //dec colum suivante
-  } else {
-    return _.tail(arrIdx).unshift(headDec);
+  const arrIdxl = _.cloneDeep(arrIdx);
+  const maxIdxl = _.cloneDeep(maxIdx);
+  const headDec = arrIdxl[0] - 1;
+  if (_.size(arrIdxl) === 1) {
+    return [headDec];
   }
-
-}
+  if (headDec < 0) {
+    const tailIdx = _.tail(arrIdxl);
+    const tailMaxIdx = _.tail(arrIdxl);
+    const tailRes = decArrayIndex(tailIdx, tailMaxIdx);
+    tailRes.unshift(maxIdxl[0]);
+    return tailRes;
+  }
+  _.tail(arrIdxl).unshift(headDec);
+  return arrIdxl;
+};
 
 const calculateAltSizeTable = (listOfList) => {
   if (_.isEmpty(listOfList)) return null;
-  const maxIdx = _.map(listOfList, n => _.size(listOfList)-1);
-  var idx = maxIdx;
+  const maxIdx = _.map(listOfList, n => _.size(n) - 1);
+  let idx = maxIdx;
   const size = _.size(listOfList);
   const idxZero = _.fill(Array(size), 0);
-  var result = [];
+  const result = [];
 
-  //todo neg boolean
+  // todo neg boolean
   while (_.isEqual(idx, idxZero)) {
     const list = getArrInArr(idx, listOfList);
     const sumOfList = sumSize(list);
@@ -99,8 +101,7 @@ const calculateAltSizeTable = (listOfList) => {
   }
 
   return result;
-
-}
+};
 
 const pickAlternateValue = {
   pickLongestSize,
@@ -111,7 +112,7 @@ const pickAlternateValue = {
   minLengthOfTemplate,
   getArrInArr,
   decArrayIndex,
-  calculateAltSizeTable
-}
+  calculateAltSizeTable,
+};
 
-module.exports=pickAlternateValue
+module.exports = pickAlternateValue;
--- test/index.js
+++ test/index.js
@@ -1,126 +1,130 @@
-import test from 'tape'
-import pav from '../src'
-const a = 'A'
-const b = 'B'
-const c = 'C'
-const a31 = ['aaa','a']
-const a123 = ['a','aa','aaa']
-const a2135 = ['aa','a','aaa','aaaaa']
+import test from 'tape';
+import pav from '../src';
+
+
+const a = 'A';
+const b = 'B';
+const c = 'C';
+const a31 = ['aaa', 'a'];
+const a123 = ['a', 'aa', 'aaa'];
+const a2135 = ['aa', 'a', 'aaa', 'aaaaa'];
 
 
 test('pick the longest size', (t) => {
-  t.plan(10)
-  t.equal(pav.pickLongestSize(null), null, 'null')
-  t.equal(pav.pickLongestSize([]), null, 'empty')
-  t.equal(pav.pickLongestSize(null, 'def123'), 'def123', 'null')
-  t.equal(pav.pickLongestSize([null, '']), '', 'discard null')
-  t.equal(pav.pickLongestSize(['ab','a','abcd'], null, 2),'ab', 'with max')
-  t.equal(pav.pickLongestSize(['ab','a','abcd']),'abcd', 'A')
-  t.equal(pav.pickLongestSize(['abcde','a','ab']),'abcde', 'B')
-  t.equal(pav.pickLongestSize(['ab','ab1','ab2','a']),'ab1', 'take 1st of 2 eq')
-  t.deepEqual(pav.pickLongestSize([[1,2],[1,2,3],[1]]),[1,2,3], 'arr of array')
-  t.deepEqual(pav.pickLongestSize([{a,b},{a,b,c},{a}]),{a,b,c}, 'array of obj')
-})
+  t.plan(10);
+  t.equal(pav.pickLongestSize(null), null, 'null');
+  t.equal(pav.pickLongestSize([]), null, 'empty');
+  t.equal(pav.pickLongestSize(null, 'def123'), 'def123', 'null');
+  t.equal(pav.pickLongestSize([null, '']), '', 'discard null');
+  t.equal(pav.pickLongestSize(['ab', 'a', 'abcd'], null, 2), 'ab', 'with max');
+  t.equal(pav.pickLongestSize(['ab', 'a', 'abcd']), 'abcd', 'A');
+  t.equal(pav.pickLongestSize(['abcde', 'a', 'ab']), 'abcde', 'B');
+  t.equal(pav.pickLongestSize(['ab', 'ab1', 'ab2', 'a']), 'ab1', 'take 1st of 2 eq');
+  t.deepEqual(pav.pickLongestSize([[1, 2], [1, 2, 3], [1]]), [1, 2, 3], 'arr of array');
+  t.deepEqual(pav.pickLongestSize([{ a, b }, { a, b, c }, { a }]), { a, b, c }, 'array of obj');
+});
 
 test('pick the shortest size', (t) => {
-  t.plan(10)
-  t.equal(pav.pickShortestSize(null), null, 'null')
-  t.equal(pav.pickShortestSize([]), null, 'empty')
-  t.equal(pav.pickShortestSize([], 'def123'), 'def123', 'empty')
-  t.equal(pav.pickShortestSize([null, '', 'abc']),'', 'discard null')
-  t.equal(pav.pickShortestSize(['ab','a','abcd']),'a', 'A')
-  t.equal(pav.pickShortestSize(['ab','a','abcd'], null, 2),'ab', 'with min')
-  t.equal(pav.pickShortestSize(['abcde','a','ab']),'a', 'B')
-  t.equal(pav.pickShortestSize(['abd','a1','a2','abc']),'a1', 'take 1st of 2 eq')
-  t.deepEqual(pav.pickShortestSize([[1,2],[1,2,3],[1]]),[1], 'arr of array')
-  t.deepEqual(pav.pickShortestSize([{a,b},{a,b,c},{a}]),{a}, 'array of obj')
-})
+  t.plan(10);
+  t.equal(pav.pickShortestSize(null), null, 'null');
+  t.equal(pav.pickShortestSize([]), null, 'empty');
+  t.equal(pav.pickShortestSize([], 'def123'), 'def123', 'empty');
+  t.equal(pav.pickShortestSize([null, '', 'abc']), '', 'discard null');
+  t.equal(pav.pickShortestSize(['ab', 'a', 'abcd']), 'a', 'A');
+  t.equal(pav.pickShortestSize(['ab', 'a', 'abcd'], null, 2), 'ab', 'with min');
+  t.equal(pav.pickShortestSize(['abcde', 'a', 'ab']), 'a', 'B');
+  t.equal(pav.pickShortestSize(['abd', 'a1', 'a2', 'abc']), 'a1', 'take 1st of 2 eq');
+  t.deepEqual(pav.pickShortestSize([[1, 2], [1, 2, 3], [1]]), [1], 'arr of array');
+  t.deepEqual(pav.pickShortestSize([{ a, b }, { a, b, c }, { a }]), { a }, 'array of obj');
+});
 
 
 test('sum the size', (t) => {
-  t.plan(6)
-  t.equal(pav.sumSize(null), 0, 'zero')
-  t.equal(pav.sumSize([]), 0, 'empty')
-  t.equal(pav.sumSize(['ab','a','abcd']),7, 'A')
-  t.equal(pav.sumSize(['ab','a',null]),3, 'with null')
-  t.deepEqual(pav.sumSize([[1,2],[1,2,3],[1]]),6, 'arr of array')
-  t.deepEqual(pav.sumSize([{a,b},{a,b,c},{a}]),6, 'array of obj')
-})
+  t.plan(6);
+  t.equal(pav.sumSize(null), 0, 'zero');
+  t.equal(pav.sumSize([]), 0, 'empty');
+  t.equal(pav.sumSize(['ab', 'a', 'abcd']), 7, 'A');
+  t.equal(pav.sumSize(['ab', 'a', null]), 3, 'with null');
+  t.deepEqual(pav.sumSize([[1, 2], [1, 2, 3], [1]]), 6, 'arr of array');
+  t.deepEqual(pav.sumSize([{ a, b }, { a, b, c }, { a }]), 6, 'array of obj');
+});
 
 test('max the size', (t) => {
-  t.plan(6)
-  t.equal(pav.maxSize(null), null, 'zero')
-  t.equal(pav.maxSize([]), null, 'empty')
-  t.equal(pav.maxSize(['ab','a','abcd']),4, 'A')
-  t.equal(pav.maxSize(['ab','a',null]),2, 'with null')
-  t.deepEqual(pav.maxSize([[1,2],[1,2,3],[1]]),3, 'arr of array')
-  t.deepEqual(pav.maxSize([{a,b},{a,b,c},{a}]),3, 'array of obj')
-})
+  t.plan(6);
+  t.equal(pav.maxSize(null), null, 'zero');
+  t.equal(pav.maxSize([]), null, 'empty');
+  t.equal(pav.maxSize(['ab', 'a', 'abcd']), 4, 'A');
+  t.equal(pav.maxSize(['ab', 'a', null]), 2, 'with null');
+  t.deepEqual(pav.maxSize([[1, 2], [1, 2, 3], [1]]), 3, 'arr of array');
+  t.deepEqual(pav.maxSize([{ a, b }, { a, b, c }, { a }]), 3, 'array of obj');
+});
 
 test('min the size', (t) => {
-  t.plan(6)
-  t.equal(pav.minSize(null), null, 'zero')
-  t.equal(pav.minSize([]), null, 'empty')
-  t.equal(pav.minSize(['ab','a','abcd']),1, 'A')
-  t.equal(pav.minSize(['ab','a',null]),1, 'with null')
-  t.deepEqual(pav.minSize([[1,2],[1,2,3],[1]]),1, 'arr of array')
-  t.deepEqual(pav.minSize([{a,b},{a,b,c},{a}]),1, 'array of obj')
-})
+  t.plan(6);
+  t.equal(pav.minSize(null), null, 'zero');
+  t.equal(pav.minSize([]), null, 'empty');
+  t.equal(pav.minSize(['ab', 'a', 'abcd']), 1, 'A');
+  t.equal(pav.minSize(['ab', 'a', null]), 1, 'with null');
+  t.deepEqual(pav.minSize([[1, 2], [1, 2, 3], [1]]), 1, 'arr of array');
+  t.deepEqual(pav.minSize([{ a, b }, { a, b, c }, { a }]), 1, 'array of obj');
+});
 
 test('minimum length of template', (t) => {
-  t.plan(2)
-  t.equal(pav.minLengthOfTemplate(null), null, 'with null')
-  t.equal(pav.minLengthOfTemplate('12345${ user }6${a.b.c}7'), 7, 'templ size')
-})
+  t.plan(2);
+  const p = '${';
+  t.equal(pav.minLengthOfTemplate(null), null, 'with null');
+  t.equal(pav.minLengthOfTemplate(`12345${p} user }6${p}a.b.c}7`, 7,
+  'templ size'));
+});
 
 test('get an array inside an array', (t) => {
-  t.plan(3)
-  t.deepEqual(pav.getArrInArr([0, 0, 0], [a31,a123,a2135]),
-   [a31[0], a123[0], a2135[0]], 'first')
-  t.deepEqual(pav.getArrInArr([1, 1, 1], [a31,a123,a2135]),
-    [a31[1], a123[1], a2135[1]], 'second')
-  t.deepEqual(pav.getArrInArr([5, 7, 8], [a31,a123,a2135]),
-     null, 'none')
-})
+  t.plan(3);
+  t.deepEqual(pav.getArrInArr([0, 0, 0], [a31, a123, a2135]),
+   [a31[0], a123[0], a2135[0]], 'first');
+  t.deepEqual(pav.getArrInArr([1, 1, 1], [a31, a123, a2135]),
+    [a31[1], a123[1], a2135[1]], 'second');
+  t.deepEqual(pav.getArrInArr([5, 7, 8], [a31, a123, a2135]),
+     null, 'none');
+});
 
 test('decrement an index of array starting from head', (t) => {
-  t.plan(5)
-  t.deepEqual(pav.decArrayIndex([3, 2], [3, 2]), [2, 2], 'A')
-  t.deepEqual(pav.decArrayIndex([0, 2], [3, 2]), [3, 1], 'B')
-  t.deepEqual(pav.decArrayIndex([0, 1], [3, 2]), [3, 0], 'C')
-  t.deepEqual(pav.decArrayIndex([1, 0], [3, 2]), [0, 0], 'D')
-  t.deepEqual(pav.decArrayIndex([0, 0], [3, 2]), null, 'E')
-
-})
+  t.plan(5);
+  pav.decArrayIndex([3, 2], [3, 2]);
+  t.deepEqual(pav.decArrayIndex([3, 2], [3, 2]), [2, 2], 'A');
+  t.deepEqual(pav.decArrayIndex([0, 2], [3, 2]), [3, 1], 'B');
+  t.deepEqual(pav.decArrayIndex([0, 1], [3, 2]), [3, 0], 'C');
+  t.deepEqual(pav.decArrayIndex([1, 0], [3, 2]), [0, 0], 'D');
+  t.deepEqual(pav.decArrayIndex([0, 0], [3, 2]), [3, -1], 'E');
+});
 
 
 test('calculate the alternative size table', (t) => {
-  t.plan(1)
+  t.plan(1);
   const expected = [
-    [3+1+2, 0, [0, 0, 0]],
-    [3+1+1], 0, [0, 0, 1],
-    [3+1+3], 0, [0, 0, 2],
-    [3+1+5], 0, [0, 0, 3],
-    [3+2+2, 0, [0, 1, 0]],
-    [3+2+1], 0, [0, 1, 1],
-    [3+2+3], 0, [0, 1, 2],
-    [3+2+5], 0, [0, 1, 3]
-    [3+3+2, 0, [0, 2, 0]],
-    [3+3+1], 0, [0, 2, 1],
-    [3+3+3], 0, [0, 2, 2],
-    [3+3+5], 0, [0, 2, 3],
-    [1+1+2, 0, [1, 0, 0]],
-    [1+1+1], 0, [1, 0, 1],
-    [1+1+3], 0, [1, 0, 2],
-    [1+1+5], 0, [1, 0, 3],
-    [1+2+2, 0, [1, 1, 0]],
-    [1+2+1], 0, [1, 1, 1],
-    [1+2+3], 0, [1, 1, 2],
-    [1+2+5], 0, [1, 1, 3]
-    [1+3+2, 0, [1, 2, 0]],
-    [1+3+1], 0, [1, 2, 1],
-    [1+3+3], 0, [1, 2, 2],
-    [1+3+5], 0, [1, 2, 3]
-  ]
-  t.deepEqual(pav.calculateAltSizeTable([a31, a123, a2135]),expected, 'Table')
-})
+    [3 + 1 + 2, 0, [0, 0, 0]],
+    [3 + 1 + 1], 0, [0, 0, 1],
+    [3 + 1 + 3], 0, [0, 0, 2],
+    [3 + 1 + 5], 0, [0, 0, 3],
+    [3 + 2 + 2, 0, [0, 1, 0]],
+    [3 + 2 + 1], 0, [0, 1, 1],
+    [3 + 2 + 3], 0, [0, 1, 2],
+    [3 + 2 + 5], 0, [0, 1, 3],
+    [3 + 3 + 2], 0, [0, 2, 0],
+    [3 + 3 + 1], 0, [0, 2, 1],
+    [3 + 3 + 3], 0, [0, 2, 2],
+    [3 + 3 + 5], 0, [0, 2, 3],
+    [1 + 1 + 2, 0, [1, 0, 0]],
+    [1 + 1 + 1], 0, [1, 0, 1],
+    [1 + 1 + 3], 0, [1, 0, 2],
+    [1 + 1 + 5], 0, [1, 0, 3],
+    [1 + 2 + 2, 0, [1, 1, 0]],
+    [1 + 2 + 1], 0, [1, 1, 1],
+    [1 + 2 + 3], 0, [1, 1, 2],
+    [1 + 2 + 5], 0, [1, 1, 3],
+    [1 + 3 + 2], 0, [1, 2, 0],
+    [1 + 3 + 1], 0, [1, 2, 1],
+    [1 + 3 + 3], 0, [1, 2, 2],
+    [1 + 3 + 5], 0, [1, 2, 3],
+  ];
+  t.deepEqual(pav.calculateAltSizeTable([a31, a123, a2135]), expected, 'Table');
+});
